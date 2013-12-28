e1af0e57051c80ac5912c92597342ba25d4ab7ea
komahan <komahan.cool@gmail.com>
2013-12-28 18:31:09 -0500

    All kprobs are automated

--- save.sh
+++ save.sh
@@ -1,6 +1,6 @@
-read -p "Enter kprob value:" i
-cp Opt.his kprob$i/
-cp HISTG* kprob$i/
-cp screen kprob$i/
-cp fort.* kprob$i/
-echo "Success saving the results into kprob"$i
+#read -p "Enter prob value:" i
+cp Opt.his kprob4/
+cp HISTG* kprob4/
+cp screen kprob4/
+cp fort.* kprob4/
+echo "Success saving the results into kprob4"
